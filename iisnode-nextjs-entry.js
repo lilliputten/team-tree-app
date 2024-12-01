c9909a367c578f876f010d12244a079b4c2afa3d
lilliputten <lilliputten@gmail.com>
2024-12-01 08:05:04 +0300

    Added configuration and start entry for iisnode IIS extension.

---
+++ iisnode-nextjs-entry.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+// NOTE: This file should be excluded from linters
+
+/**
+ * @desc Script to launch the nodejs server for iisnode extension.
+ */
+
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = process.env.port || 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev }); // Here i tried including and removing port and hostname with same result
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+      // TODO?
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query);
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query);
+      } else {
+        await handle(req, res, parsedUrl);
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  })
+    .once('error', (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
